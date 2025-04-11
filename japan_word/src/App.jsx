1cdfa2620bc432b67df23969c01361cde1743221
yiyaaa <thdtjs1219@gmail.com>
2025-04-11 22:06:44 +0900

    git init

---
+++ japan_word/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='font-bold'>
+        시작해볼까요?
+      </div>
+    </>
+  )
+}
+
+export default App
